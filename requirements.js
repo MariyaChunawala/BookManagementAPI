bc3dd52040afeb393c3b56803ce54521482f718d
unknown <mariyachunawala7@gmail.com>
2022-07-28 16:13:51 +0530

    More Requirements are added

--- requirements.js
+++ requirements.js
@@ -5,22 +5,60 @@
 // Book
 // ISBN, Title, Author [], Language, Pub Date, No. of pages, Category []
 // Why we need an API?
+// 
+// 1. GET Method
 // to get all books [Done]
 // to get specific books [Done]
 // to get a list of books based on category [Done]
 // to get a list of books based on author [Done]
+// 
+// 2. POST Method
+// To post a new book
+// 
+// 3. PUT Method
+// Update book details
+// update/add new author
+// 
+// 4. DELETE Method
+// Delete a book
+// Delete an author from a book
+
 
 // Author 
 // Name, Id, Book []
 // Why we need an API?
+// 
+// 1. GET Method
 // to get all authors [Done]
 // to get specific author [Done]
 // to get a list of authors based on book [Done]
+// 
+// 2. POST Method
+// to post a new Author
+// 
+// 3. PUT Method
+// Update author details
+// 
+// 4. DELETE Method
+// Delete an Author
 
 
 // publications
 // Name, Id, Book[]
 // Why we need an API?
+// 
+// 1. GET Methods
 // to get all publications [Done]
 // to get specific publication [Done]
 // to get a list of publications based on book [Done]
+// 
+// 2. Post Method
+// Add new Publication
+// 
+// 3. PUT Method
+// Update Publication details
+// Udpate/add new book to a publications
+// 
+// 4. DELETE Method
+// Delete a book from publication
+// Delete a publication
